c5419f0d51fd627bcbc9b53c8774aac4fff77e8b
Giovane Verbinnen <giovane.oliveira@funcionalconsultoria.com.br>
2021-07-01 11:01:54 -0300

    aula-4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <div class="filters flex space-x-6">
         <div class="w-1/3 ">
             <select name="category" id="category" class="w-full rounded-xl px-4 py-2 border-none">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,9 +48,58 @@
        </header>
 
        <main class="container mx-auto flex max-w-6xl">
-           <div class="w-4/12 mr-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tenetur quos expedita laboriosam ratione alias illum voluptatibus earum accusamus soluta ea, iure et ad eligendi, quidem explicabo aliquam in minus odit ullam corrupti ex cupiditate? Cum enim porro ratione commodi illum doloribus laboriosam at minima, illo expedita aspernatur itaque consequuntur dolore odio quae eos ab unde a, saepe nulla reprehenderit! Numquam natus est veritatis? Nam dolores incidunt magni labore quo. Magni corrupti rem alias repellat repellendus explicabo, illum laboriosam amet porro quo totam pariatur deleniti voluptatibus iusto autem exercitationem, consequuntur earum? Accusamus, et. Beatae laboriosam dolorum adipisci nemo quisquam molestias.
+        <div class="mr-8">
+            <div class="bg-white border border-blue-500 rounded-xl mt-16" style=" border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+            border-image-slice: 1;
+            background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+            background-origin: border-box;
+            background-clip: content-box, border-box;">
+            <div class="text-center px-6 py-2 pt-6 ">
+                <h3 class="font-semibold text-base">Add an idea</h3>
+                <p class="text-xs mt-4">Let us know what you wold like and we'll take a look over!</p>
+                <form action="#" method="POST" class="space-y-4 px-2 py-6">
+                    <div>
+                        <input type="text" class="w-full bg-gray-100 rounded-xl placeholder-gray-900 border-none text-sm" placeholder="Your idea">
+                    </div>
+                    <div>
+                        <select name="category_add" id="category_add" class="w-full text-sm rounded-xl px-4 py-2 border-none bg-gray-100" >
+                            <option value="one">Other Filters</option>
+                            <option value="one">one</option>
+                            <option value="one">one</option>
+                            <option value="one">one</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2 border-none" placeholder="Describe your idea"></textarea>
+                    </div>
+                    <div class="flex items-center justify-between space-x-3">
+                        <button
+                            type="button"
+                            class="
+                            flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200
+                            font-bold rounded-xl border border-gray-200 hover:border-gray-400 hover:bg-gray-300
+                            transition duration-150 ease-in px-6 py-3
+                            ">
+                            <svg class="h-4 w-4 text-gray-600 transform-gpu -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="ml-1">Attach</span>
+                    </button>
+                    <button
+                    type="submit"
+                    class="
+                    flex items-center justify-center w-1/2 h-11 text-xs bg-blue-500 text-white
+                    font-bold rounded-xl border border-blue-200 hover:border-blue-400 hover:bg-blue-600
+                    transition duration-150 ease-in px-6 py-3
+                    ">
+                    <span class="ml-1">Submit</span>
+                    </button>
+                    </div>
+                </form>
             </div>
+        </div>
+        </div>
+
             <div class="w-8/12">
                 <nav class="flex items-center justify-between text-xs " >
                     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10 transition-all">
@@ -64,6 +113,7 @@
 
                     </ul>
                 </nav>
+
                 <div class="mt-8">
                     {{ $slot }}
                 </div>
